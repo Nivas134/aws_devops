f9a1f8a47106b66d39576f7d11eca28d99efad26
Nivas134 <kalyankumar767399@gmail.com>
2026-07-03 14:08:58 +0530

    added appspec and container files

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+docker pull kalyan133/aws_cicd
+
+docker run -d -p 8080:8080 kalyan133/aws_cicd
+
+# Start the container with the specified image and options
+echo "Starting container..."
---
+++ scripts/stop_container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# stop the container with the specified name
+echo "Stopping container..."

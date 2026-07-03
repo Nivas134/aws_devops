dffa0b3358bdf60dbd45ee61b700b771c93f2d3b
Nivas134 <kalyankumar767399@gmail.com>
2026-07-03 14:30:04 +0530

    final deploy test changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,4 @@ docker pull kalyan133/aws_cicd
 docker run -d -p 8080:8080 kalyan133/aws_cicd
 
 # Start the container with the specified image and options
-echo "Starting container..."
+echo "Starting container...1"
